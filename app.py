7b92bcd3ce57c76fd73bbdbca92c1e40176ee63f
Udit Kumar <55291327+uditkumar01@users.noreply.github.com>
2021-02-01 23:00:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,44 @@
 from telethon import TelegramClient, events, sync
 from telethon.tl.types import InputChannel, InputPeerUser
-# import yaml
+import yaml
 import sys
+from telethon.sessions import StringSession
 import logging
-import configparser
-import json
+import  configparser
+import  json, requests
 from threading import Thread
-from telethon import TelegramClient, events
-from telethon . errors import SessionPasswordNeededError
+from  telethon  import  TelegramClient , events
+from  telethon . errors  import  SessionPasswordNeededError
 import os
-import telethon . sync
+import  telethon . sync
 import subprocess
-import requests
-import json
 
-from pprint import pprint
+from  pprint  import  pprint
+        
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logging.getLogger('telethon').setLevel(level=logging.WARNING)
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("telethon").setLevel(level=logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
 def start(config):
-    client = TelegramClient(config["session_name"],
-                            config["api_id"],
-                            config["api_hash"])
+    client = TelegramClient(
+        StringSession(config["session_name"]), config["api_id"], config["api_hash"]
+    )
     client.start()
 
     @client.on(events.NewMessage(pattern="/py"))
-    async def add_output(event):
-        if str(event.sender_id) == os.environ.get("abhishek") or str(event.sender_id) == os.environ.get("udit") or str(event.sender_id) == os.environ.get("piyush"):
+    async def add_py(event):
+        if str(event.sender_id) == "503607829" or str(event.sender_id) == "1255422851" or str(event.sender_id) == "1449695176":
 
             msg = event.message.message
-            await event.reply("Running command . . .")
+            await event.reply("Running python command . . .")
             try:
                 code = msg.lstrip('/py')
             except IndexError:
-                await event.reply("Running command . . .")
+                await event.reply("No arguments given (Py)")
             command = "".join(f"\n {x}" for x in code.split("\n.strip()"))
             indexofip = command.find("py_input_vars")
             ip=""
@@ -48,7 +49,6 @@ def start(config):
             # skipcq: BAN-B603
             """
                 print(input())
-
                 py_input_vars:
                 1 2 3 4 5
                 3 4 5 6 7
@@ -74,18 +74,19 @@ def start(config):
                     await event.reply(result)
                 except TelegramError as excp:
                     await event.reply(str(excp.message))
-
-
+        else:
+            await event.reply("I only execute EdwardROCKS Commands, So Sorry")
+            
     @client.on(events.NewMessage(pattern="/c"))
-    async def add_output(event):
-        if str(event.sender_id) == os.environ.get("abhishek") or str(event.sender_id) == os.environ.get("udit") or str(event.sender_id) == os.environ.get("piyush"):
+    async def add_c(event):
+        if str(event.sender_id) == "503607829" or str(event.sender_id) == "1255422851" or str(event.sender_id) == "1449695176":
 
             msg = event.message.message
-            await event.reply("Running command . . .")
+            await event.reply("Running c command . . .")
             try:
                 code = msg.lstrip('/c')
             except IndexError:
-                await event.reply("Running command . . .")
+                await event.reply("No arguments given (c)")
             command = "".join(f"\n {x}" for x in code.split("\n.strip()"))
             indexofip = command.find("c_input_vars")
             ip=""
@@ -96,12 +97,9 @@ def start(config):
             # skipcq: BAN-B603
             """
                 printf("hello");
-
                 c_input_vars:
-                3
-                1
-                2
-                3
+                1 2 3 4 5
+                3 4 5 6 7
                 
             """
 
@@ -128,15 +126,15 @@ def start(config):
 
 
     @client.on(events.NewMessage(pattern="/ac"))
-    async def add_output(event):
-        if str(event.sender_id) == os.environ.get("abhishek") or str(event.sender_id) == os.environ.get("udit") or str(event.sender_id) == os.environ.get("piyush"):
+    async def add_cpp(event):
+        if str(event.sender_id) == "503607829" or str(event.sender_id) == "1255422851" or str(event.sender_id) == "1449695176":
 
             msg = event.message.message
-            await event.reply("Running command . . .")
+            await event.reply("Running cpp command . . .")
             try:
                 code = msg.lstrip('/ac')
             except IndexError:
-                await event.reply("Running command . . .")
+                await event.reply("No arguments given (cpp)")
             command = "".join(f"\n {x}" for x in code.split("\n.strip()"))
             indexofip = command.find("cpp_input_vars")
             ip=""
@@ -147,7 +145,6 @@ def start(config):
             # skipcq: BAN-B603
             """
                 cout << "hello" << endl;
-
                 cpp_input_vars:
                 1 2 3 4 5
                 3 4 5 6 7
@@ -176,12 +173,11 @@ def start(config):
 
     client.run_until_disconnected()
 
-
 if __name__ == "__main__":
-
     config = {
         "api_id": os.environ.get("api_id"),
         "api_hash": os.environ.get("api_hash"),
-        "session_name": os.environ.get("session_name")
+        "session_name": os.environ.get("session_name"),
     }
+
     start(config)
