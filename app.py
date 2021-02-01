43b894e1f1501b2b89abc7a1d37459260081e021
Udit Kumar <55291327+uditkumar01@users.noreply.github.com>
2021-02-02 01:34:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,27 +22,28 @@ logging.basicConfig(
 logging.getLogger("telethon").setLevel(level=logging.WARNING)
 logger = logging.getLogger(__name__)
 
-
 def start(config):
     client = TelegramClient(
-        StringSession(config["session_name"]), config["api_id"], config["api_hash"]
+        StringSession(config["session_name"]
+                      ), config["api_id"], config["api_hash"]
     )
     client.start()
 
     @client.on(events.NewMessage(pattern="/py"))
     async def add_py(event):
-        if str(event.sender_id) == "503607829" or str(event.sender_id) == "1255422851" or str(event.sender_id) == "1449695176":
+        if str(event.sender_id) in os.environ.get("sender_id").split(','):
 
             msg = event.message.message
-            await event.reply("Running python command . . .")
+            await client.edit_message(event.from_id, event.id, msg+"\n\n"+"__"+"Running python command . . .__")
             try:
                 code = msg.lstrip('/py')
             except IndexError:
-                await event.reply("No arguments given (Py)")
+                await client.edit_message(event.from_id, event.id, msg+"\n\n"+"__"+"No arguments given (py) ...__")
+            
             command = "".join(f"\n {x}" for x in code.split("\n.strip()"))
             indexofip = command.find("py_input_vars")
-            ip=""
-            if indexofip!=-1:
+            ip = ""
+            if indexofip != -1:
                 user_in = command[indexofip+len("py_input_vars:"):].strip()
                 command = command[:indexofip]
                 ip = user_in.encode("utf-8")
@@ -64,33 +65,78 @@ def start(config):
                 "https://compilethis.herokuapp.com/", data=data).content).get("result")
 
             if len(result) > 2500:
-                await event.reply("Results too big... Sending as file")
+                await client.edit_message(event.from_id, event.id, msg+"\n\n"+"__"+"Result is too big .. send as file__")
+                with open("output.txt", "w+") as f:
+                    f.write(result)
+                await client.send_file(event.chat_id, 'output.txt')
+                os.remove("output.txt")
+            else:
+                # time.sleep(2)
+                try:
+                    await client.edit_message(event.from_id,event.id,msg+"\n\n"+"="*[20,max(len(result),12)][len(result)<20]+"\n"+"__OUTPUT:__\n**"+result+"**")
+                except Exception as excp:
+                    await client.edit_message(event.from_id,event.id,msg+"\n\n"+"="*[20,max(len(excp),12)][len(excp)<20]+"\n"+"__ERROR:__\n**"+excp+"**")
+
+    @client.on(events.NewMessage(pattern="/ac"))
+    async def add_cpp(event):
+        if str(event.sender_id) in os.environ.get("sender_id").split(','):
+
+            msg = event.message.message
+            await client.edit_message(event.from_id, event.id, msg+"\n\n"+"__"+"Running cpp command . . ."+"__")
+            try:
+                code = msg.lstrip('/ac')
+            except IndexError:
+                await client.edit_message(event.from_id, event.id, msg+"\n\n"+"__"+"No arguments given (cpp) ...__")
+            command = "".join(f"\n {x}" for x in code.split("\n.strip()"))
+            indexofip = command.find("cpp_input_vars")
+            ip = ""
+            if indexofip != -1:
+                user_in = command[indexofip+len("cpp_input_vars:"):].strip()
+                command = command[:indexofip]
+                ip = user_in.encode("utf-8")
+            # skipcq: BAN-B603
+            """
+                cout << "hello" << endl;
+                cpp_input_vars:
+                1 2 3 4 5
+                3 4 5 6 7
+            """
+
+            data = {
+                "lang": "cpp",
+                "code": command,
+                "input": ip
+            }
+
+            result = json.loads(requests.post(
+                "https://compilethis.herokuapp.com/", data=data).content).get("result")
+
+            if len(result) > 2500:
+                await client.edit_message(event.from_id, event.id, msg+"\n\n"+"__"+"Result is too big .. send as file__")
                 with open("output.txt", "w+") as f:
                     f.write(result)
                 await client.send_file(event.chat_id, 'output.txt')
                 os.remove("output.txt")
             else:
                 try:
-                    await event.reply(result)
-                except TelegramError as excp:
-                    await event.reply(str(excp.message))
-        else:
-            await event.reply("I only execute EdwardROCKS Commands, So Sorry")
-            
-    @client.on(events.NewMessage(pattern="/c"))
+                    await client.edit_message(event.from_id,event.id,msg+"\n\n"+"="*[20,max(len(result),12)][len(result)<20]+"\n"+"__OUTPUT:__\n**"+result+"**")
+                except Exception as excp:
+                    await client.edit_message(event.from_id,event.id,msg+"\n\n"+"="*[20,max(len(excp),12)][len(excp)<20]+"\n"+"__ERROR:__\n**"+excp+"**")
+
+    @client.on(events.NewMessage(pattern="/c |/c\n"))
     async def add_c(event):
-        if str(event.sender_id) == "503607829" or str(event.sender_id) == "1255422851" or str(event.sender_id) == "1449695176":
+        if str(event.sender_id) in os.environ.get("sender_id").split(','):
 
             msg = event.message.message
-            await event.reply("Running c command . . .")
+            await client.edit_message(event.from_id, event.id, msg+"\n\n"+"__"+"Running c command . . ."+"__")
             try:
                 code = msg.lstrip('/c')
             except IndexError:
-                await event.reply("No arguments given (c)")
+                await client.edit_message(event.from_id, event.id, msg+"\n\n"+"__"+"No arguments given (c) ...__")
             command = "".join(f"\n {x}" for x in code.split("\n.strip()"))
             indexofip = command.find("c_input_vars")
-            ip=""
-            if indexofip!=-1:
+            ip = ""
+            if indexofip != -1:
                 user_in = command[indexofip+len("c_input_vars:"):].strip()
                 command = command[:indexofip]
                 ip = user_in.encode("utf-8")
@@ -113,63 +159,18 @@ def start(config):
                 "https://compilethis.herokuapp.com/", data=data).content).get("result")
 
             if len(result) > 2500:
-                await event.reply("Results too big... Sending as file")
+                await client.edit_message(event.from_id, event.id, msg+"\n\n"+"__"+"Result is too big .. send as file__")
                 with open("output.txt", "w+") as f:
                     f.write(result)
                 await client.send_file(event.chat_id, 'output.txt')
                 os.remove("output.txt")
             else:
                 try:
-                    await event.reply(result)
-                except TelegramError as excp:
-                    await event.reply(str(excp.message))
+                    await client.edit_message(event.from_id,event.id,msg+"\n\n"+"="*[20,max(len(result),12)][len(result)<20]+"\n"+"__OUTPUT:__\n**"+result+"**")
+                except Exception as excp:
+                    await client.edit_message(event.from_id,event.id,msg+"\n\n"+"="*[20,max(len(excp),12)][len(excp)<20]+"\n"+"__ERROR:__\n**"+excp+"**")
 
-
-    @client.on(events.NewMessage(pattern="/ac"))
-    async def add_cpp(event):
-        if str(event.sender_id) == "503607829" or str(event.sender_id) == "1255422851" or str(event.sender_id) == "1449695176":
-
-            msg = event.message.message
-            await event.reply("Running cpp command . . .")
-            try:
-                code = msg.lstrip('/ac')
-            except IndexError:
-                await event.reply("No arguments given (cpp)")
-            command = "".join(f"\n {x}" for x in code.split("\n.strip()"))
-            indexofip = command.find("cpp_input_vars")
-            ip=""
-            if indexofip!=-1:
-                user_in = command[indexofip+len("cpp_input_vars:"):].strip()
-                command = command[:indexofip]
-                ip = user_in.encode("utf-8")
-            # skipcq: BAN-B603
-            """
-                cout << "hello" << endl;
-                cpp_input_vars:
-                1 2 3 4 5
-                3 4 5 6 7
-            """
-
-            data = {
-                "lang": "cpp",
-                "code": command,
-                "input": ip
-            }
-
-            result = json.loads(requests.post(
-                "https://compilethis.herokuapp.com/", data=data).content).get("result")
-
-            if len(result) > 2500:
-                await event.reply("Results too big... Sending as file")
-                with open("output.txt", "w+") as f:
-                    f.write(result)
-                await client.send_file(event.chat_id, 'output.txt')
-                os.remove("output.txt")
-            else:
-                try:
-                    await event.reply(result)
-                except TelegramError as excp:
-                    await event.reply(str(excp.message))
+    
 
     client.run_until_disconnected()
 
