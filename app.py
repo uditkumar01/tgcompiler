2b75b15a08d3d8f86122b2f395264983b42db84a
Udit Kumar <55291327+uditkumar01@users.noreply.github.com>
2021-02-04 12:38:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -192,16 +192,10 @@ def start(config):
                 os.remove("output.txt")
             else:
                 try:
-                    st = time-time()
-                    j = 0
-                    while(j<len(result)):
-                        print(result[j])
-                        # time.sleep(0.6)
-                        j+=1
-                    await client.edit_message(event.from_id,event.id,result[0])
+                    await client.edit_message(event.from_id,event.id,msg+"\n\n"+"="*[20,max(len(result),12)][len(result)<20]+"\n"+"__OUTPUT:__\n\n**"+str(result)+"**")
                 except Exception as excp:
                     await client.edit_message(event.from_id,event.id,msg+"\n\n"+"="*[20,max(len(str(excp)),12)][len(str(excp))<20]+"\n"+"__ERROR:__\n\n**"+str(excp)+"**")
-
+        
     
 
     client.run_until_disconnected()
