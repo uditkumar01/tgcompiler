cfdbb13b09c1dfd71801dd20d21ff0a05669f6fc
Udit Kumar <55291327+uditkumar01@users.noreply.github.com>
2021-02-05 18:37:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -218,7 +218,7 @@ def start(config):
                 await event.edit(msg+"\n\n"+"="*[20,max(len(str(excp)),12)][len(str(excp))<20]+"\n"+"__ERROR:__\n\n**"+str(excp)+"**")
     
     @client.on(events.NewMessage(pattern="/gitinfo"))
-    def git_user_info():
+    def git_user_info(event):
         
         msg = event.message.message
         await event.edit(msg+"\n\n"+"__"+"Running git info command . . ."+"__")
@@ -231,80 +231,83 @@ def start(config):
         username = command.strip()
         
         url = "https://github.com/"
+        
+        try:
+            page = urllib.request.urlopen(url+username)
 
-        page = urllib.request.urlopen(url+username)
+            soup = BeautifulSoup(page,"html.parser")
 
-        soup = BeautifulSoup(page,"html.parser")
+            result = []
+            result.append("Username: "+username)
 
-        result = []
-        result.append("Username: "+username)
-
-        no_of_repo,no_of_followers,no_of_following,bio,status,profile_img,contributions_last_year,no_of_stars = None,None,None,None,None,None,None,None
-        try:
-            no_of_repo = soup.find('span',{'class':'Counter'}).text
-            result.append("Number of repos: "+no_of_repo)
-        except:
-            pass
-        try:
-            no_of_followers = soup.find('a',{'href':'/'+username+'?tab=followers'}).find('span').text
-            result.append("Number of followers: "+no_of_followers)
-        except:
-            pass
-        try:
-            no_of_following = soup.find('a',{'href':'/'+username+'?tab=following'}).find('span').text
-            result.append("Number of following: "+no_of_following)
-        except:
-            pass
-        try:
-            no_of_stars = soup.find('a',{'href':'/'+username+'?tab=stars'}).find('span').text
-            result.append("Number of stars: "+no_of_stars)
-        except:
-            pass
-        try:
-            bio = soup.find('div',{'class':'user-profile-bio'}).find('div').text.strip().split()
-            bio_list = []
-            for bio_word in bio:
-                if bio_word.strip():
-                    bio_list.append(bio_word)
-            result.append(username+"'s bio: "+" ".join(bio_list))
-        except:
-            pass
-        try:
-            status = soup.find('div',{'class':'user-status-message-wrapper'}).find('div',{'class':''}).text.strip().split()
-            status_list = []
-            for status_word in status:
-                if status_word.strip():
-                    status_list.append(status_word)
-            result.append(username+"'s status: "+" ".join(status_list))
-        except:
-            pass
-        try:
-            profile_img = soup.find('img',{'alt':'@'+username})['src']
-            result.append("Profile pic URL: "+profile_img)
-        except:
-            pass
-        try:
-            contributions_last_year = soup.find('div',{'class':'js-yearly-contributions'}).find('h2').text.strip().split()
-            contributions_last_year_list = []
-            for contributions in contributions_last_year:
-                if contributions.strip():
-                    contributions_last_year_list.append(contributions)
-            result.append("Contributions: "+" ".join(contributions_last_year_list))
-        except:
-            pass
-        # print(no_of_repo,no_of_followers,no_of_following,bio,status,contributions_last_year,profile_img)
-        result = "\n".join(result)
-        if len(result) > 2500:
-            await event.edit(msg+"\n\n"+"__"+"Result is too big .. send as file__")
-            with open("output.txt", "w+") as f:
-                f.write(result)
-            await client.send_file(event.chat_id, 'output.txt')
-            os.remove("output.txt")
-        else:
+            no_of_repo,no_of_followers,no_of_following,bio,status,profile_img,contributions_last_year,no_of_stars = None,None,None,None,None,None,None,None
             try:
-                await event.edit(msg+"\n\n"+"="*[20,max(len(result),12)][len(result)<20]+"\n"+"__OUTPUT:__\n\n**"+result+"**")
-            except Exception as excp:
-                await event.edit(msg+"\n\n"+"="*[20,max(len(str(excp)),12)][len(str(excp))<20]+"\n"+"__ERROR:__\n\n**"+str(excp)+"**")
+                no_of_repo = soup.find('span',{'class':'Counter'}).text
+                result.append("Number of repos: "+no_of_repo)
+            except:
+                pass
+            try:
+                no_of_followers = soup.find('a',{'href':'/'+username+'?tab=followers'}).find('span').text
+                result.append("Number of followers: "+no_of_followers)
+            except:
+                pass
+            try:
+                no_of_following = soup.find('a',{'href':'/'+username+'?tab=following'}).find('span').text
+                result.append("Number of following: "+no_of_following)
+            except:
+                pass
+            try:
+                no_of_stars = soup.find('a',{'href':'/'+username+'?tab=stars'}).find('span').text
+                result.append("Number of stars: "+no_of_stars)
+            except:
+                pass
+            try:
+                bio = soup.find('div',{'class':'user-profile-bio'}).find('div').text.strip().split()
+                bio_list = []
+                for bio_word in bio:
+                    if bio_word.strip():
+                        bio_list.append(bio_word)
+                result.append(username+"'s bio: "+" ".join(bio_list))
+            except:
+                pass
+            try:
+                status = soup.find('div',{'class':'user-status-message-wrapper'}).find('div',{'class':''}).text.strip().split()
+                status_list = []
+                for status_word in status:
+                    if status_word.strip():
+                        status_list.append(status_word)
+                result.append(username+"'s status: "+" ".join(status_list))
+            except:
+                pass
+            try:
+                profile_img = soup.find('img',{'alt':'@'+username})['src']
+                result.append("Profile pic URL: "+profile_img)
+            except:
+                pass
+            try:
+                contributions_last_year = soup.find('div',{'class':'js-yearly-contributions'}).find('h2').text.strip().split()
+                contributions_last_year_list = []
+                for contributions in contributions_last_year:
+                    if contributions.strip():
+                        contributions_last_year_list.append(contributions)
+                result.append("Contributions: "+" ".join(contributions_last_year_list))
+            except:
+                pass
+            # print(no_of_repo,no_of_followers,no_of_following,bio,status,contributions_last_year,profile_img)
+            result = "\n".join(result)
+            if len(result) > 2500:
+                await event.edit(msg+"\n\n"+"__"+"Result is too big .. send as file__")
+                with open("output.txt", "w+") as f:
+                    f.write(result)
+                await client.send_file(event.chat_id, 'output.txt')
+                os.remove("output.txt")
+            else:
+                try:
+                    await event.edit(msg+"\n\n"+"="*[20,max(len(result),12)][len(result)<20]+"\n"+"__OUTPUT:__\n\n**"+result+"**")
+                except Exception as excp:
+                    await event.edit(msg+"\n\n"+"="*[20,max(len(str(excp)),12)][len(str(excp))<20]+"\n"+"__ERROR:__\n\n**"+str(excp)+"**")
+        except Exception as excp:
+            await event.edit(msg+"\n\n"+"="*[20,max(len(str(excp)),12)][len(str(excp))<20]+"\n"+"__PAGE ERROR:__\n\n**"+str(excp)+"**")
 
 
     client.run_until_disconnected()
