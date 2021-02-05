d2cb5beeecad2117fa5bae6dcd8709c23cffeb9e
Udit Kumar <55291327+uditkumar01@users.noreply.github.com>
2021-02-05 13:52:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from  telethon . errors  import  SessionPasswordNeededError
 import os
 import  telethon . sync
 import subprocess
-import asyncio
+import asyncio,time
 from  pprint  import  pprint
         
 
@@ -34,7 +34,7 @@ def start(config):
         if str(event.sender_id) in os.environ.get("sender_id"):
 
             msg = event.message.message
-            await event.send_message(msg+"\n\n"+"__"+"Running python command . . .__")
+            await event.edit(msg+"\n\n"+"__"+"Running python command . . .__")
             try:
                 code = msg.lstrip('/py')
             except IndexError:
@@ -128,11 +128,11 @@ def start(config):
         if str(event.sender_id) in os.environ.get("sender_id"):
 
             msg = event.message.message
-            await client.edit_message(event.from_id, event.id, msg+"\n\n"+"__"+"Running c command . . ."+"__")
+            await event.edit(msg+"\n\n"+"__"+"Running c command . . ."+"__")
             try:
                 code = msg.lstrip('/c')
             except IndexError:
-                await client.edit_message(event.from_id, event.id, msg+"\n\n"+"__"+"No arguments given (c) ...__")
+                await event.edit(msg+"\n\n"+"__"+"No arguments given (c) ...__")
             command = "".join(f"\n {x}" for x in code.split("\n.strip()"))
             indexofip = command.find("c_input_vars")
             ip = ""
@@ -159,16 +159,16 @@ def start(config):
                 "https://compilethis.herokuapp.com/", data=data).content).get("result")
 
             if len(result) > 2500:
-                await client.edit_message(event.from_id, event.id, msg+"\n\n"+"__"+"Result is too big .. send as file__")
+                await event.edit(msg+"\n\n"+"__"+"Result is too big .. send as file__")
                 with open("output.txt", "w+") as f:
                     f.write(result)
                 await client.send_file(event.chat_id, 'output.txt')
                 os.remove("output.txt")
             else:
                 try:
-                    await client.edit_message(event.from_id,event.id,msg+"\n\n"+"="*[20,max(len(result),12)][len(result)<20]+"\n"+"__OUTPUT:__\n\n**"+result+"**")
+                    await event.edit(msg+"\n\n"+"="*[20,max(len(result),12)][len(result)<20]+"\n"+"__OUTPUT:__\n\n**"+result+"**")
                 except Exception as excp:
-                    await client.edit_message(event.from_id,event.id,msg+"\n\n"+"="*[20,max(len(str(excp)),12)][len(str(excp))<20]+"\n"+"__ERROR:__\n\n**"+str(excp)+"**")
+                    await event.edit(msg+"\n\n"+"="*[20,max(len(str(excp)),12)][len(str(excp))<20]+"\n"+"__ERROR:__\n\n**"+str(excp)+"**")
         
     @client.on(events.NewMessage(pattern="/animate"))
     async def text_list_animate(event):
