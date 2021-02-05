56e98a10889314c87db3f7f6f50ee780c8ee966f
Udit Kumar <55291327+uditkumar01@users.noreply.github.com>
2021-02-05 14:00:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,191 +31,191 @@ def start(config):
 
     @client.on(events.NewMessage(pattern="/py"))
     async def add_py(event):
-        if str(event.sender_id) in os.environ.get("sender_id"):
+#         if str(event.sender_id) in os.environ.get("sender_id"):
 
-            msg = event.message.message
-            await event.edit(msg+"\n\n"+"__"+"Running python command . . .__")
+        msg = event.message.message
+        await event.edit(msg+"\n\n"+"__"+"Running python command . . .__")
+        try:
+            code = msg.lstrip('/py')
+        except IndexError:
+            await event.edit(msg+"\n\n"+"__"+"No arguments given (py) ...__")
+
+        command = "".join(f"\n {x}" for x in code.split("\n.strip()"))
+        indexofip = command.find("py_input_vars")
+        ip = ""
+        if indexofip != -1:
+            user_in = command[indexofip+len("py_input_vars:"):].strip()
+            command = command[:indexofip]
+            ip = user_in.encode("utf-8")
+        # skipcq: BAN-B603
+        """
+            print(input())
+            py_input_vars:
+            1 2 3 4 5
+            3 4 5 6 7
+        """
+
+        data = {
+            "lang": "python3",
+            "code": command,
+            "input": ip
+        }
+
+        result = json.loads(requests.post(
+            "https://compilethis.herokuapp.com/", data=data).content).get("result")
+
+        if len(result) > 2500:
+            await event.edit(msg+"\n\n"+"__"+"Result is too big .. send as file__")
+            with open("output.txt", "w+") as f:
+                f.write(result)
+            await client.send_file(event.chat_id, 'output.txt')
+            os.remove("output.txt")
+        else:
+            # time.sleep(2)
             try:
-                code = msg.lstrip('/py')
-            except IndexError:
-                await event.edit(msg+"\n\n"+"__"+"No arguments given (py) ...__")
-            
-            command = "".join(f"\n {x}" for x in code.split("\n.strip()"))
-            indexofip = command.find("py_input_vars")
-            ip = ""
-            if indexofip != -1:
-                user_in = command[indexofip+len("py_input_vars:"):].strip()
-                command = command[:indexofip]
-                ip = user_in.encode("utf-8")
-            # skipcq: BAN-B603
-            """
-                print(input())
-                py_input_vars:
-                1 2 3 4 5
-                3 4 5 6 7
-            """
-
-            data = {
-                "lang": "python3",
-                "code": command,
-                "input": ip
-            }
-
-            result = json.loads(requests.post(
-                "https://compilethis.herokuapp.com/", data=data).content).get("result")
-
-            if len(result) > 2500:
-                await event.edit(msg+"\n\n"+"__"+"Result is too big .. send as file__")
-                with open("output.txt", "w+") as f:
-                    f.write(result)
-                await client.send_file(event.chat_id, 'output.txt')
-                os.remove("output.txt")
-            else:
-                # time.sleep(2)
-                try:
-                    await event.edit(msg+"\n\n"+"="*[20,max(len(result),12)][len(result)<20]+"\n"+"__OUTPUT:__\n\n**"+result+"**")
-                except Exception as excp:
-                    await event.edit(msg+"\n\n"+"="*[20,max(len(str(excp)),12)][len(str(excp))<20]+"\n"+"__ERROR:__\n\n**"+str(excp)+"**")
+                await event.edit(msg+"\n\n"+"="*[20,max(len(result),12)][len(result)<20]+"\n"+"__OUTPUT:__\n\n**"+result+"**")
+            except Exception as excp:
+                await event.edit(msg+"\n\n"+"="*[20,max(len(str(excp)),12)][len(str(excp))<20]+"\n"+"__ERROR:__\n\n**"+str(excp)+"**")
 
     @client.on(events.NewMessage(pattern="/ac"))
     async def add_cpp(event):
-        if str(event.sender_id) in os.environ.get("sender_id"):
+#         if str(event.sender_id) in os.environ.get("sender_id"):
 
-            msg = event.message.message
-            await event.edit(msg+"\n\n"+"__"+"Running cpp command . . ."+"__")
+        msg = event.message.message
+        await event.edit(msg+"\n\n"+"__"+"Running cpp command . . ."+"__")
+        try:
+            code = msg.lstrip('/ac')
+        except IndexError:
+            await event.edit(msg+"\n\n"+"__"+"No arguments given (cpp) ...__")
+        command = "".join(f"\n {x}" for x in code.split("\n.strip()"))
+        indexofip = command.find("cpp_input_vars")
+        ip = ""
+        if indexofip != -1:
+            user_in = command[indexofip+len("cpp_input_vars:"):].strip()
+            command = command[:indexofip]
+            ip = user_in.encode("utf-8")
+        # skipcq: BAN-B603
+        """
+            cout << "hello" << endl;
+            cpp_input_vars:
+            1 2 3 4 5
+            3 4 5 6 7
+        """
+
+        data = {
+            "lang": "cpp",
+            "code": command,
+            "input": ip
+        }
+
+        result = json.loads(requests.post(
+            "https://compilethis.herokuapp.com/", data=data).content).get("result")
+
+        if len(result) > 2500:
+            await event.edit(msg+"\n\n"+"__"+"Result is too big .. send as file__")
+            with open("output.txt", "w+") as f:
+                f.write(result)
+            await client.send_file(event.chat_id, 'output.txt')
+            os.remove("output.txt")
+        else:
             try:
-                code = msg.lstrip('/ac')
-            except IndexError:
-                await event.edit(msg+"\n\n"+"__"+"No arguments given (cpp) ...__")
-            command = "".join(f"\n {x}" for x in code.split("\n.strip()"))
-            indexofip = command.find("cpp_input_vars")
-            ip = ""
-            if indexofip != -1:
-                user_in = command[indexofip+len("cpp_input_vars:"):].strip()
-                command = command[:indexofip]
-                ip = user_in.encode("utf-8")
-            # skipcq: BAN-B603
-            """
-                cout << "hello" << endl;
-                cpp_input_vars:
-                1 2 3 4 5
-                3 4 5 6 7
-            """
-
-            data = {
-                "lang": "cpp",
-                "code": command,
-                "input": ip
-            }
-
-            result = json.loads(requests.post(
-                "https://compilethis.herokuapp.com/", data=data).content).get("result")
-
-            if len(result) > 2500:
-                await event.edit(msg+"\n\n"+"__"+"Result is too big .. send as file__")
-                with open("output.txt", "w+") as f:
-                    f.write(result)
-                await client.send_file(event.chat_id, 'output.txt')
-                os.remove("output.txt")
-            else:
-                try:
-                    await event.edit(msg+"\n\n"+"="*[20,max(len(result),12)][len(result)<20]+"\n"+"__OUTPUT:__\n\n**"+result+"**")
-                except Exception as excp:
-                    await event.edit(msg+"\n\n"+"="*[20,max(len(str(excp)),12)][len(str(excp))<20]+"\n"+"__ERROR:__\n\n**"+str(excp)+"**")
+                await event.edit(msg+"\n\n"+"="*[20,max(len(result),12)][len(result)<20]+"\n"+"__OUTPUT:__\n\n**"+result+"**")
+            except Exception as excp:
+                await event.edit(msg+"\n\n"+"="*[20,max(len(str(excp)),12)][len(str(excp))<20]+"\n"+"__ERROR:__\n\n**"+str(excp)+"**")
 
     @client.on(events.NewMessage(pattern="/c"))
     async def add_c(event):
-        if str(event.sender_id) in os.environ.get("sender_id"):
+#         if str(event.sender_id) in os.environ.get("sender_id"):
 
-            msg = event.message.message
-            await event.edit(msg+"\n\n"+"__"+"Running c command . . ."+"__")
+        msg = event.message.message
+        await event.edit(msg+"\n\n"+"__"+"Running c command . . ."+"__")
+        try:
+            code = msg.lstrip('/c')
+        except IndexError:
+            await event.edit(msg+"\n\n"+"__"+"No arguments given (c) ...__")
+        command = "".join(f"\n {x}" for x in code.split("\n.strip()"))
+        indexofip = command.find("c_input_vars")
+        ip = ""
+        if indexofip != -1:
+            user_in = command[indexofip+len("c_input_vars:"):].strip()
+            command = command[:indexofip]
+            ip = user_in.encode("utf-8")
+        # skipcq: BAN-B603
+        """
+            printf("hello");
+            c_input_vars:
+            1 2 3 4 5
+            3 4 5 6 7
+
+        """
+
+        data = {
+            "lang": "c",
+            "code": command,
+            "input": ip
+        }
+
+        result = json.loads(requests.post(
+            "https://compilethis.herokuapp.com/", data=data).content).get("result")
+
+        if len(result) > 2500:
+            await event.edit(msg+"\n\n"+"__"+"Result is too big .. send as file__")
+            with open("output.txt", "w+") as f:
+                f.write(result)
+            await client.send_file(event.chat_id, 'output.txt')
+            os.remove("output.txt")
+        else:
             try:
-                code = msg.lstrip('/c')
-            except IndexError:
-                await event.edit(msg+"\n\n"+"__"+"No arguments given (c) ...__")
-            command = "".join(f"\n {x}" for x in code.split("\n.strip()"))
-            indexofip = command.find("c_input_vars")
-            ip = ""
-            if indexofip != -1:
-                user_in = command[indexofip+len("c_input_vars:"):].strip()
-                command = command[:indexofip]
-                ip = user_in.encode("utf-8")
-            # skipcq: BAN-B603
-            """
-                printf("hello");
-                c_input_vars:
-                1 2 3 4 5
-                3 4 5 6 7
-                
-            """
+                await event.edit(msg+"\n\n"+"="*[20,max(len(result),12)][len(result)<20]+"\n"+"__OUTPUT:__\n\n**"+result+"**")
+            except Exception as excp:
+                await event.edit(msg+"\n\n"+"="*[20,max(len(str(excp)),12)][len(str(excp))<20]+"\n"+"__ERROR:__\n\n**"+str(excp)+"**")
 
-            data = {
-                "lang": "c",
-                "code": command,
-                "input": ip
-            }
-
-            result = json.loads(requests.post(
-                "https://compilethis.herokuapp.com/", data=data).content).get("result")
-
-            if len(result) > 2500:
-                await event.edit(msg+"\n\n"+"__"+"Result is too big .. send as file__")
-                with open("output.txt", "w+") as f:
-                    f.write(result)
-                await client.send_file(event.chat_id, 'output.txt')
-                os.remove("output.txt")
-            else:
-                try:
-                    await event.edit(msg+"\n\n"+"="*[20,max(len(result),12)][len(result)<20]+"\n"+"__OUTPUT:__\n\n**"+result+"**")
-                except Exception as excp:
-                    await event.edit(msg+"\n\n"+"="*[20,max(len(str(excp)),12)][len(str(excp))<20]+"\n"+"__ERROR:__\n\n**"+str(excp)+"**")
-        
     @client.on(events.NewMessage(pattern="/animate"))
     async def text_list_animate(event):
-        if str(event.sender_id) in os.environ.get("sender_id"):
-            print("*"*20)
-            print("event",event)
-            print("from_id",event.from_id)
-            print("id",event.id)
-            print("*"*20)
-            msg = event.message.message
-            await event.edit(msg+"\n\n"+"__"+"Running animate command . . ."+"__")
+#         if str(event.sender_id) in os.environ.get("sender_id"):
+#             print("*"*20)
+#             print("event",event)
+#             print("from_id",event.from_id)
+#             print("id",event.id)
+#             print("*"*20)
+        msg = event.message.message
+        await event.edit(msg+"\n\n"+"__"+"Running animate command . . ."+"__")
+        try:
+            code = msg.lstrip('/animate')
+        except IndexError:
+            await event.edit(msg+"\n\n"+"__"+"No arguments given (c) ...__")
+        command = "".join(f"\n {x}" for x in code.split("\n.strip()"))
+        time_out = 60
+        result = []
+        if '-t' in command:
+            result = (command.split('-t')[0]).split(',')
             try:
-                code = msg.lstrip('/animate')
-            except IndexError:
-                await event.edit(msg+"\n\n"+"__"+"No arguments given (c) ...__")
-            command = "".join(f"\n {x}" for x in code.split("\n.strip()"))
-            time_out = 60
-            result = []
-            if '-t' in command:
-                result = (command.split('-t')[0]).split(',')
-                try:
-                    time_out = int(command.split('-t')[1].strip())
-                except:
-                    time_out = 60
-            else:
-                result = command.split(',')
-            result = list(map(lambda x:x.strip(),result))
-            print(result)
-            if len(result) > 2500:
-                await event.edit(msg+"\n\n"+"__"+"Result is too big .. send as file__")
-                with open("output.txt", "w+") as f:
-                    f.write(result)
-                await client.send_file(event.chat_id, 'output.txt')
-                os.remove("output.txt")
-            else:
-                try:
-                    indexofresult,st = 0,time.time()
-                    while(time.time()-st<time_out):
-                        await event.edit(str(result[indexofresult]))
-                        time.sleep(0.6)
-                        indexofresult+=1
-                        if indexofresult>=len(result):
-                            indexofresult = 0
-                except Exception as excp:
-                    await event.edit(msg+"\n\n"+"="*[20,max(len(str(excp)),12)][len(str(excp))<20]+"\n"+"__ERROR:__\n\n**"+str(excp)+"**")
-        
-    
+                time_out = int(command.split('-t')[1].strip())
+            except:
+                time_out = 60
+        else:
+            result = command.split(',')
+        result = list(map(lambda x:x.strip(),result))
+        print(result)
+        if len(result) > 2500:
+            await event.edit(msg+"\n\n"+"__"+"Result is too big .. send as file__")
+            with open("output.txt", "w+") as f:
+                f.write(result)
+            await client.send_file(event.chat_id, 'output.txt')
+            os.remove("output.txt")
+        else:
+            try:
+                indexofresult,st = 0,time.time()
+                while(time.time()-st<time_out):
+                    await event.edit(str(result[indexofresult]))
+                    time.sleep(0.6)
+                    indexofresult+=1
+                    if indexofresult>=len(result):
+                        indexofresult = 0
+            except Exception as excp:
+                await event.edit(msg+"\n\n"+"="*[20,max(len(str(excp)),12)][len(str(excp))<20]+"\n"+"__ERROR:__\n\n**"+str(excp)+"**")
+
+
 
     client.run_until_disconnected()
 
